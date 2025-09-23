8d9dd1265cf321a769482c3780fd1a01441ad7d4
KarynaMisnik <karinamisnik94@gmail.com>
2025-09-23 20:48:34 +0300

    main tree is created, testing components
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

---
+++ components/navbar.js
@@ -0,0 +1,25 @@
+class MyNavbar extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.innerHTML = `
+      <style>
+        nav {
+          background: #333;
+          padding: 10px;
+        }
+        nav a {
+          color: white;
+          margin-right: 15px;
+          text-decoration: none;
+        }
+      </style>
+      <nav>
+        <a href="index.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="contact.html">Contact</a>
+      </nav>
+    `;
+  }
+}
+customElements.define("page-navbar", MyNavbar);
